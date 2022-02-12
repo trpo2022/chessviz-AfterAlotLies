e700ddfac3c2aca77834f575aebd52de8072b00b
Vyacheslav <guzxaroh@gmail.com>
2022-02-12 22:33:03 +0700

    Changed file creating chess desk

--- chess.c
+++ chess.c
@@ -79,8 +79,11 @@ int main ()
 	for(int i = 0; i < 9; i++)
 		{
 			for(int j = 0; j < 9; j++)
-				printf("%c ", mass[i][j]);
-				printf("\n");
+				{	
+					printf("%c ", mass[i][j]);
+		  		}
+		   printf("\n");
 		}	
+		
 	return 0;
 }
